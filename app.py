beef169a250671e0958de44840c31e561607895b
micah <142721110+MikiAsefa@users.noreply.github.com>
2023-10-07 15:48:00 +0300

    emoji for "Time Released"

--- app.py
+++ app.py
@@ -123,10 +123,11 @@ def get_issue_info(issue):
         time_difference_string = ""
 
     release_time_string = release_time.strftime('%Y-%m-%d %H:%M %p')
+    CLOCK_EMOJI = chr(0x23F0)
     if time_difference_string:
-        release_time_message = f"\nTime released: {release_time_string} ({time_difference_string})\n"
+        release_time_message = f"\n{CLOCK_EMOJI}Time released: {release_time_string} ({time_difference_string})\n"
     else:
-        release_time_message = f"\nTime released: {release_time_string}\n"
+        release_time_message = f"\n{CLOCK_EMOJI}Time released: {release_time_string}\n"
 
     if issue_tags is not None and len(issue_tags) > 0:
         issue_tags_strings = []
